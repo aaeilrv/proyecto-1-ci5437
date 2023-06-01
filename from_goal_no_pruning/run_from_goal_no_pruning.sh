f269d7a7ffac865adffcb954797db7b1fb68260d
Daniel Robayo <87443087+danielrobapaz@users.noreply.github.com>
2023-05-31 21:26:28 -0400

    bimbadabum

--- from_goal_no_pruning/run_from_goal_no_pruning.sh
+++ from_goal_no_pruning/run_from_goal_no_pruning.sh
@@ -1,10 +1,21 @@
 #!bin/bash
 
-./15_puzzle.from_goal_no_pruning
-./24_puzzle.from_goal_no_pruning
+#./15_puzzle.from_goal_no_pruning
+#./24_puzzle.from_goal_no_pruning
+echo "\nrunning hanoi4_12"
 ./hanoi4_12.from_goal_no_pruning
+
+echo "\nrunning hanoi4_14"
 ./hanoi4_14.from_goal_no_pruning
+
+echo "\nrunning hanoi4_17"
 ./hanoi4_17.from_goal_no_pruning
+
+echo "\nrunning topspin12_4"
 ./topspin12_4.from_goal_no_pruning
+
+echo "\nrunning topspin14_4"
 ./topspin14_4.from_goal_no_pruning
+
+echo "\nrunning topspin17_4"
 ./topspin17_4.from_goal_no_pruning
