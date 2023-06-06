83f213c012cffa4c10c3345ed48396b11cdae7b4
aaeilrv <16-11233@usb.ve>
2023-06-06 13:00:30 -0400

    listo IDA?? who knows

--- algorithms/ida_star.cpp
+++ algorithms/ida_star.cpp
@@ -12,42 +12,50 @@
 #include <utility>
 #include <limits>
 #include "priority_queue.hpp"
+#include "heuristic.hpp"
 
 #define MAX_STR_LEN 999
 
 using namespace std;
 
+pair<state_t*, int> f_bounded_dfs_visit(state_t &state, unsigned long int bound, int cost, int hist) {
 
-
-pair<state_t*, int> f_bounded_dfs_visit(state_t n, unsigned long int bound) {
     // base cases
-    int f = n.g + h(n.state);
-    if (f > bound) {
-        return make_pair(nullptr, f);
+    int f = cost + g.getHeuristic(state);
+
+    if (f > bound) return make_pair(nullptr, f);
+
+    if (is_goal(&state)) {
+        printf("goal reached");
+        return make_pair(state, f);
     }
 
-    if (is_goal(n.state)) {
-        return make_pair(n.state, f);
+    int t = INT_MAX;
+
+    int rule_id;
+    ruleid_iterator_t iter;
+
+    pair<state_t, int> p;
+    state_t m;
+
+    init_fwd_iter(&iter, &state);
+
+    while ((rule_id = next_ruleid(&iter)) >= 0) {
+        apply_fwd_rule(rule_id, &state, &m);
+
+        int new_cost = cost + get_fwd_rule_cost(rule_id);
+
+        p = f_bounded_dfs_visit(m, bound, new_cost, next_fwd_history(hist, rule_id));
+
+        if (p.first != nullptr) return p;
+
+        t = min(t, p.second);
     }
 
-    float t = numeric_limits<float>::infinity();
-
-    for (auto succ : n.state.succ()) {
-        state_t m = n.make_state_t(succ.first, succ.second);
-
-        pair<state_t, unsigned> p = f_bounded_dfs_visit(m, bound);
-
-        if (p.first != nullptr) {
-            return p;
-        }
-
-        if (t > p.second) {
-            t = p.second;
-        }
-    }
+    free(m);
 
     return make_pair(nullptr, t);
-    
+
 }
 
 
@@ -75,7 +83,7 @@ int main(int argc, char **argv) {
 
     // search with increasing f-value bounds
     while (1) {
-        make_pair(state_t, unsigned long int) p = f_bounded_dfs_visit(root, bound);
+        make_pair(state_t, unsigned long int) p = f_bounded_dfs_visit(root, bound, 0, init_history);
 
         if (p.first != nullptr) {
             return p.first;
