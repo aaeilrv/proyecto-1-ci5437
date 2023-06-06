611b9c53bd46c3e4bbc8b4d96b4aaf6ab0e1a666
Daniel Robayo <87443087+danielrobapaz@users.noreply.github.com>
2023-06-05 23:08:28 -0400

    ya no quiero jugar a la inteligencia artifial

--- algorithms/bfs.cpp
+++ algorithms/bfs.cpp
@@ -2,11 +2,7 @@
 // and implicit representation for the graphs
 #include <stdlib.h>
 #include <stdio.h>
-#include <inttypes.h>
-#include <assert.h>
 #include <queue>    // queue library
-#include <ctime>
-#define  MAX_LINE_LENGTH 999 
 
 using namespace std;
 
@@ -14,53 +10,36 @@ void bread_first_search(state_t *state) {
     state_t curr_state, child_state; 
     queue<state_t> q; // fifo
     ruleid_iterator_t iter;
-    clock_t start, end;
-    unsigned long int n_child;
-    float elapsed; 
-    int ruleid, expected; 
+    unsigned long int n_child, n_child_at_depth;
+    int ruleid, curr_depth = 0;
 
-    // start counting the time
-    start = clock();
     q.push(*state);
     
-    // initialize excepcted 
-    expected = 1; // this variable is to show the elapsed time and the nodes genereated so far every 10 seconds
-
-    n_child = 0;
 
     while (!q.empty()) {
-        curr_state = q.front();
-        q.pop();
+        n_child_at_depth = q.size();
+        n_child = 0;
 
-        init_bwd_iter(&iter, &curr_state);
+
         // loop over the successor of curr_state
-        while ( (ruleid = next_ruleid(&iter)) >= 0) {
-            apply_bwd_rule(ruleid, &curr_state, &child_state);
-            q.push(child_state);
+        for (int i = 0; i < n_child_at_depth; i++) {
+            curr_state = q.front();
+            q.pop();
+            init_bwd_iter(&iter, &curr_state);
+            while ((ruleid = next_ruleid(&iter)) >= 0) {
+                apply_bwd_rule(ruleid, &curr_state, &child_state);
+                q.push(child_state);
 
-            n_child++;
+                n_child++;
+            }
         }
 
-        end = clock();
-
-        elapsed = (end - start)/(double)CLOCKS_PER_SEC;
-
-        // show elapsed time every 10 seconds
-        if ((int)elapsed / 10 == expected) {
-            printf("elapsed time: %f s, nodes generated = %ld \n", elapsed, n_child);
-            expected++;
-        }
-
-        if (elapsed >= 60) {
-            printf("nodes generated per second = %f\n", n_child/elapsed);
-            return;
-        }
+        printf("depth %d - nodes generated: %ld - branch factor %.2f\n", curr_depth, n_child_at_depth, (float)n_child/n_child_at_depth);
+        curr_depth++;
     }
 }
 
 int main(int argc, char **argv) {
-    char str[MAX_LINE_LENGTH+1];
-    ssize_t nchars;
     state_t state_0;
     int goal_num;
 
