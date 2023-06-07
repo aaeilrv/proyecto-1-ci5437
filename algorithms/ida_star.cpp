d199490762ec789882b9aceaccc6a9e186ad5804
aaeilrv <16-11233@usb.ve>
2023-06-06 23:08:45 -0400

    ida

--- algorithms/ida_star.cpp
+++ algorithms/ida_star.cpp
@@ -12,40 +12,50 @@
 #include <utility>
 #include <limits>
 #include "priority_queue.hpp"
+#include "heuristic.hpp"
 
 #define MAX_STR_LEN 999
 
 using namespace std;
 
+pair<state_t*, int> f_bounded_dfs_visit(state_t &state, unsigned long int bound, int cost, int hist) {
 
-pair<state_t*, int> f_bounded_dfs_visit(state_t n, unsigned long int bound) {
     // base cases
-    int f = n.g + h(n.state);
-    if (f > bound) {
-        return make_pair(nullptr, f);
+    int f = cost + g.getHeuristic(state);
+
+    if (f > bound) return make_pair(nullptr, f);
+
+    if (is_goal(&state)) {
+        printf("goal reached");
+        return make_pair(state, f);
     }
 
-    if (is_goal(n.state)) {
-        return make_pair(n.state, f);
-    }
+    int t = INT_MAX;
 
-    float t = numeric_limits<float>::infinity();
+    int rule_id;
+    ruleid_iterator_t iter;
 
-    for (auto succ : n.state.succ()) {
-        state_t m = n.make_state_t(succ.first, succ.second);
+    pair<state_t, int> p;
+    state_t m;
 
-        pair<state_t, unsigned> p = f_bounded_dfs_visit(m, bound);
+    init_fwd_iter(&iter, &state);
 
-        if (p.first != nullptr) {
-            return p;
-        }
+    while ((rule_id = next_ruleid(&iter)) >= 0) {
+        apply_fwd_rule(rule_id, &state, &m);
+
+        int new_cost = cost + get_fwd_rule_cost(rule_id);
+
+        p = f_bounded_dfs_visit(m, bound, new_cost, next_fwd_history(hist, rule_id));
+
+        if (p.first != nullptr) return p;
 
         t = min(t, p.second);
-
     }
 
+    free(m);
+
     return make_pair(nullptr, t);
-    
+
 }
 
 
@@ -56,7 +66,7 @@ int main(int argc, char **argv) {
     char state_str[MAX_STR_LEN + 1];
     ssize_t nchars;
     state_t init_state;
-    unsigned long int bound = h(init_state);
+    unsigned long int bound = heuristic(init_state);
 
     // read
     printf("enter init state: ");
@@ -73,7 +83,7 @@ int main(int argc, char **argv) {
 
     // search with increasing f-value bounds
     while (1) {
-        make_pair(state_t, unsigned long int) p = f_bounded_dfs_visit(root, bound);
+        make_pair(state_t, unsigned long int) p = f_bounded_dfs_visit(root, bound, 0, init_history);
 
         if (p.first != nullptr) {
             return p.first;
